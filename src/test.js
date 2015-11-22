c56fe22707ea9e88d12a4e4ec91c7ce6b076d16f
dp <dp@dealerwebb.com>
2015-11-22 12:14:33 +0530

    * test js file

---
+++ src/test.js
@@ -0,0 +1,16 @@
+describe("the let keyword", function () {
+
+    it("The  let keyword", function () {
+
+        function test() {
+            for (var i; i < 10; i++) {
+                console.log(i);
+            }
+            i = 10;
+            expect(i).toBe(10);
+        }
+
+        test();
+
+    });
+});
